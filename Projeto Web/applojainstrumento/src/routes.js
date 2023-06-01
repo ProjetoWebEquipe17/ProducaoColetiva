75c129f3371c009d82358843f454bd38a009509b
ProjetoWebEquipe17 <francisco.oalourenco@senacsp.edu.br>
2023-05-31 22:55:00 -0300

    Entrega do dia 01/06
    Entrega da tarefa do dia 01/06

--- Projeto Web/applojainstrumento/src/routes.js
+++ Projeto Web/applojainstrumento/src/routes.js
@@ -10,6 +10,23 @@ import Vendas from './pages/Vendas';
 import Clientes from './pages/Clientes';
 import Erro from './pages/Erro';
 
+import { Router, Request, Response } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
+
+const router = Router();
+
+//note que podemos tipar (typescript): req e do tipo "Request" e res é do tipo Response
+router.get('/teste', (req: Request, res: Response) => {
+
+return res.json({ ok: "Chamou a rota" })
+
+})
+
+//note que podemos tipar (typescript): req e do tipo "Request" e res é do tipo Response
+router.post('/users', new CreateUserController().handle)
+
+export { router };
+
 function RouterApp(){
     return(
         <BrowserRouter>
